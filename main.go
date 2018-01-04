9618fb1757c3665743c57626be97c427abb1dea8
Colin Benner <colin@yzhs.de>
2018-01-04 09:39:54 +0100

    Fill in the blanks
    The program is currently capable of replacing the existing Linux kernel
    tree it finds at ~/src/linux with the latest version, committing any
    modified files to git. It does also keep the .config file and backs it
    up to ~/prj/linux_config/config. It does not, however, Add new source
    files to version control, nor does it run make <anything> on the new
    source tree.

--- main.go
+++ main.go
@@ -15,6 +15,14 @@ const (
 	URL        = "https://kernel.org/"
 )
 
+func run(name string, arg ...string) {
+	cmd := exec.Command(name, arg...)
+	if err := cmd.Run(); err != nil {
+		panic(err)
+	}
+}
+
+// Check which version is in ~/src/linux
 func getLocalVersion() semver.Version {
 	if err := os.Chdir(os.ExpandEnv(SOURCE_DIR)); err != nil {
 		panic(err)
@@ -23,7 +31,8 @@ func getLocalVersion() semver.Version {
 	cmd := exec.Command("make", "kernelversion")
 	bytes, err := cmd.Output()
 	if err != nil {
-		panic(err)
+		version, _ := semver.Make("4.0.0")
+		return version
 	}
 
 	localVersion, err := semver.Make(strings.TrimSpace(string(bytes)))
@@ -34,6 +43,8 @@ func getLocalVersion() semver.Version {
 	return localVersion
 }
 
+// Look for the latest version on kernel.org and get the link for downloading
+// the source archive for that version.
 func getLatestVersion() (semver.Version, string) {
 	doc, err := goquery.NewDocument(URL)
 	if err != nil {
@@ -57,26 +68,69 @@ func getLatestVersion() (semver.Version, string) {
 	return latestVersion, url
 }
 
-func cleanRepository() {
+func backupConfig() {
+	run("mv", ".config", os.ExpandEnv("$HOME/prj/linux_config/config"))
 }
 
-func extractNewVersion() {
+// Download and extract the new sources
+func extractNewVersion(url string) {
+	if err := os.Chdir(".."); err != nil {
+		panic(err)
+	}
+	curl := exec.Command("curl", "-s", url)
+	tar := exec.Command("tar", "xhJ")
+
+	var err error
+	tar.Stdin, err = curl.StdoutPipe()
+	if err != nil {
+		panic(err)
+	}
+	if err = tar.Start(); err != nil {
+		panic(err)
+	}
+	if err = curl.Run(); err != nil {
+		panic(err)
+	}
+	if err = tar.Wait(); err != nil {
+		panic(err)
+	}
+}
+
+func replaceSourceDirectory(newVersion semver.Version) {
+	name := "linux-" + newVersion.String()
+
+	run("mv", "linux/.git", "linux/.config", name)
+	run("trash-put", "-r", "linux")
+	run("mv", name, "linux")
+	if err := os.Chdir("linux"); err != nil {
+		panic(err)
+	}
+}
+
+func updateGitRepo(newVersion semver.Version) {
+	run("git", "commit", "-am", "Add kernel "+newVersion.String())
 }
 
 func main() {
 	localVersion := getLocalVersion()
 	latestVersion, url := getLatestVersion()
 
-	log.Println("The current local version is", localVersion)
-	log.Println("The latest version is", latestVersion)
-
 	if localVersion.LT(latestVersion) {
-		log.Println("Updating to the newest version")
+		log.Println("The current local version is", localVersion, "while the latest version is", latestVersion)
+		log.Println("Updating...")
 
-		log.Println("Cleaning the source directory")
-		cleanRepository()
+		log.Println("Backing up config to ~/prj/linux_config")
+		backupConfig()
 
 		log.Println("Downloading the latest version")
-		extractNewVersion()
+		extractNewVersion(url)
+
+		log.Println("Replacing the source directory with the latest version")
+		replaceSourceDirectory(latestVersion)
+
+		log.Println("Adding files to version control")
+		updateGitRepo(latestVersion)
+	} else {
+		log.Printf("The local version (%v) is up to date\n", localVersion)
 	}
 }
